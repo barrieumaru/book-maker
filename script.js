fa1e640c9a54f953c13ae87cde192711aca01f02
Barrie Umaru <umaru.barrie@student.vives.be>
2021-08-02 20:27:10 +0200

    second commit

--- script.js
+++ script.js
@@ -78,8 +78,8 @@ function fetchBookmarks() {
     // Create bookmarks array in localStorage
     bookmarks = [
       {
-        name: 'Jacinto Design',
-        url: 'http://jacinto.design',
+        name: 'count-down',
+        url: 'https://barrieumaru.github.io/Custom-Countdown/',
       },
     ];
     localStorage.setItem('bookmarks', JSON.stringify(bookmarks));
